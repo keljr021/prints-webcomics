92133dff2b3ceb4e5565d177e5129af80ddd6d8d
Kelvin <keljr021@gmail.com>
2024-08-21 10:47:35 -0400

    Add Login state for navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,10 +16,19 @@
       <template #end>
           <b-navbar-item tag="div">
             <b-input class="px-4" placeholder="Search" v-model="searchQuery" @keyup.native.enter="handleSearch" />
-            <div class="buttons">
-                <b-button type="is-primary" tag="router-link" to="/account/new" outlined>Create Account</b-button>
-                <b-button type="is-primary" outlined>Sign In</b-button>
-              </div>
+            <div class="buttons" style="display:none">
+              <b-button type="is-primary" tag="router-link" to="/account/new" outlined>Create Account</b-button>
+              <b-button type="is-primary" outlined>Sign In</b-button>
+            </div>
+            <b-field size="is-medium">
+              <b-select v-model="accountValue" @input="handleSelectClick" placeholder="My account" class="account">
+                  <option value="dashboard">Dashboard</option>
+                  <option value="favorites">Favorites</option>
+                  <option value="history">History</option>
+                  <option value="account">Account</option>
+                  <option value="/">Logout</option>
+              </b-select>
+            </b-field>
           </b-navbar-item>
       </template>
   </b-navbar>
@@ -37,6 +46,12 @@ export default {
   methods: {
     handleSearch() {
       return this.$router.push({ name: 'search', params: { query: this.searchQuery } });
+    },
+    handleSelectClick() {
+      if (this.accountValue === '/')
+        return this.$router.push({ path: '/' });
+      
+        return this.$router.push({ name: this.accountValue });
     }
   }
 }
@@ -60,5 +75,13 @@ export default {
 
 .logo {
   max-height: 45px;
+}
+
+.account {
+    width: 200px;
+}
+
+.account option {
+  padding: 5px 0;
 }
 </style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -2,19 +2,31 @@
   <div class="history container">
     <view-header>
       <template v-slot:header>History</template>
+      <template v-slot:caption>
+        <p>These series below are recommended to you based on your reading history.</p>
+      </template>
     </view-header>
     <div>
-      x
+      <div class="columns">
+        <div class="column">
+          <search-item />
+          <search-item />
+          <search-item />
+          <search-item />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import ViewHeader from './../components/ViewHeader.vue';
+import SearchItem from './../components/SearchItem.vue';
 export default {
   name: 'History',
   components: {
-    ViewHeader
+    ViewHeader,
+    SearchItem
   }
 }
 </script>
